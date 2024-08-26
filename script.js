9dade24b100e15c0ea7cd0fbc45f802cae973336
MuttKitten <139673834+Muttiskitten@users.noreply.github.com>
2024-08-26 16:48:58 -0400

    script.js
    attempting to make a floating box with all info about a hovered over word on the index page

---
+++ script.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.info').forEach(function(trigger) {
+  trigger.addEventListener('mouseover', function(event) {
+    var infoContent = this.getAttribute('data-info');
+    document.getElementById('info-content').textContent = infoContent;
+
+    var infoBox = document.querySelector('.info-content');
+    infoBox.style.left = event.clientX + 20 + 'px';
+    infoBox.style.top = event.clientY + 'px';
+    infoBox.style.display = 'block';
+  });
+
+  trigger.addEventListener('mouseout', function(event) {
+    var infoBox = document.querySelector('.info-content');
+    infoBox.style.display = 'none';
+  });
+});
