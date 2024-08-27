67b97e9d4374ac0dfef2059517fa50dd52850929
MuttKitten <139673834+Muttiskitten@users.noreply.github.com>
2024-08-27 18:18:12 -0400

    script.js
    Added:
    - Custom line breaks in the info boxes

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ document.querySelectorAll('.info').forEach(function(trigger) {
     var infoBox = document.querySelector('.info-content');
     infoBox.style.display = 'none';
   });
+document.querySelectorAll('.info').forEach(function(trigger) {
+  trigger.addEventListener('mouseover', function(event) {
+    var infoContent = this.getAttribute('data-info');
+    var infoContentArray = infoContent.split('\n'); // Split the content into an array by newline
+    var infoContentHTML = '';
+
+    infoContentArray.forEach(function(line) {
+      infoContentHTML += '<p>' + line + '</p>'; // Wrap each line in a <p> tag
+    });
+
+    document.getElementById('info-content').innerHTML = infoContentHTML;
+
+    var infoBox = document.querySelector('.info-content');
+    infoBox.style.left = event.clientX + 20 + 'px';
+    infoBox.style.top = event.clientY + 20 + 'px';
+    infoBox.style.display = 'block';
+  });
+
+  trigger.addEventListener('mouseout', function(event) {
+    var infoBox = document.querySelector('.info-content');
+    infoBox.style.display = 'none';
+  });
 });
